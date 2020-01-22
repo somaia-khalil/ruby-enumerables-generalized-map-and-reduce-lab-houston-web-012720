7b627ca7acbe797d0638c4a21a0639e733fd278e
Somaia Khalil <sumaya_khair@live.com>
2020-01-22 02:34:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
-# Your Code Here
+require 'pry'
+person_array = [{name: "Tashawn", interests: ["sports", "tv"]}, {name: "Laura", interests: ["skiing", "books"]}, {name: "Evans", interests: ["sports", "camping"]}]
+
+def nested (data)
+  person_array.map do |person| 
+      binding.pry
+    "Hello #{person["name"]}!"
+  end
+end
+
+nested(person_array)
